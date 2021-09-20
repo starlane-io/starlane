f0139c6582e07a274f6f91dea5655509a69c0b4e
Scott Williams <scott@mightydevco.com>
2021-09-19 19:27:22 -0500

    ls is working too

--- rust/starlane/src/main.rs
+++ rust/starlane/src/main.rs
@@ -47,9 +47,8 @@ fn main() -> Result<(), Error> {
                                                             SubCommand::with_name("config").subcommands(vec![SubCommand::with_name("set-host").usage("set the host that the starlane CLI connects to").arg(Arg::with_name("hostname").required(true).help("the hostname of the starlane instance you wish to connect to")).display_order(0),
                                                                                                                             SubCommand::with_name("get-host").usage("get the host that the starlane CLI connects to")]).usage("read or manipulate the cli config").display_order(1).display_order(1),
                                                             SubCommand::with_name("publish").usage("publish an artifact bundle").args(vec![Arg::with_name("dir").required(true).help("the source directory for this bundle"),Arg::with_name("address").required(true).help("the publish address of this bundle i.e. 'space:sub_space:bundle:1.0.0'")].as_slice()),
-                                                            SubCommand::with_name("cp").usage("an artifact bundle").args(vec![Arg::with_name("src").takes_value(true).index(1).required(true).help("the source file [local file or starlane resource address]"),Arg::with_name("dst").takes_value(true).index(2).required(true).help("the source file [local file or starlane resource address]")].as_slice()),
-                                                            SubCommand::with_name("xp").usage("publish an artifact bundle"),
-                                                   SubCommand::with_name("create").usage("create a resource").setting(clap::AppSettings::TrailingVarArg).args(vec![Arg::with_name("address").required(true).help("address of your new resource"),Arg::with_name("create-args").multiple(true).required(false)].as_slice()),
+                                                            SubCommand::with_name("cp").usage("copy a file").args(vec![Arg::with_name("src").takes_value(true).index(1).required(true).help("the source file [local file or starlane resource address]"),Arg::with_name("dst").takes_value(true).index(2).required(true).help("the  destination [local file or starlane resource address]")].as_slice()),
+                                                            SubCommand::with_name("create").usage("create a resource").setting(clap::AppSettings::TrailingVarArg).args(vec![Arg::with_name("address").required(true).help("address of your new resource"),Arg::with_name("create-args").multiple(true).required(false)].as_slice()),
                                                             SubCommand::with_name("ls").usage("list resources").args(vec![Arg::with_name("address").required(true).help("the resource address to list"),Arg::with_name("child-pattern").required(false).help("a pattern describing the children to be listed .i.e '<File>' for returning resource type File")].as_slice())
     ]);
 
@@ -97,14 +96,7 @@ fn main() -> Result<(), Error> {
             cp(args.clone()).await.unwrap();
         });
         shutdown();
-    } else if let Option::Some(args) = matches.subcommand_matches("xp") {
-let rt = Runtime::new().unwrap();
-rt.block_on(async {
-xp().await.unwrap();
-});
-shutdown();
-}
-
+    }
     else if let Option::Some(args) = matches.subcommand_matches("create") {
         let rt = Runtime::new().unwrap();
         rt.block_on(async {
@@ -197,51 +189,6 @@ async fn cp(args: ArgMatches<'_>) -> Result<(), Error> {
 
         starlane_api.create_resource(create).await?;
         println!("CP DONE.");
-
-        starlane_api.shutdown();
-
-    } else {
-        unimplemented!("copy from starlane to local not yet supported")
-    }
-
-    Ok(())
-}
-
-async fn xp() -> Result<(), Error> {
-    let starlane_api = starlane_api().await?;
-
-    xp2(starlane_api.clone(),"hyperspace:starlane:fs").await?;
-    xp2(starlane_api.clone(),"hyperspace:starlane:fs2").await?;
-    Ok(())
-}
-
-async fn xp2(starlane_api: StarlaneApi,  dst: &str ) -> Result<(), Error> {
-println!("DST: {}", dst );
-    if ResourcePath::from_str(dst ).is_ok() {
-        let dst = ResourcePath::from_str(dst)?;
-        // copying from src to dst
-
-
-        let create = ResourceCreate {
-            parent: dst
-                .parent()
-                .ok_or("must have an address with a parent")?
-                .into(),
-            key: KeyCreationSrc::None,
-            address: AddressCreationSrc::Exact(dst),
-            archetype: ResourceArchetype {
-                kind: ResourceKind::FileSystem,
-                specific: None,
-                config: None,
-            },
-            state_src: AssignResourceStateSrc::Stateless,
-            registry_info: Option::None,
-            owner: Option::None,
-            strategy: ResourceCreateStrategy::CreateOrUpdate,
-        };
-
-        starlane_api.create_resource(create).await?;
-        println!("XP2 DONE.");
 
         starlane_api.shutdown();
 
