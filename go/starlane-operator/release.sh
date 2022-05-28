6a93462a0df776bd37b0f7e8679bd16cbac2bc55
Scott Williams <scott@mightydevco.com>
2022-05-28 02:05:39 -0500

    added the commit bash

---
+++ go/starlane-operator/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMG="starlane/starlane-operator:latest"
+
+make docker-build docker-push IMG=$IMG
+
+cd config/manager 
+
+kustomize edit set image controller=${IMG}
+
+cd ../..
+
+kustomize build config/default > starlane-operator.yml
+
+
