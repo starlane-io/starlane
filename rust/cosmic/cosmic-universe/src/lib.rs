4cf982f5d52d01e4d3c0d256cd51a95a43556f86
Scott Williams <scott@mightydevco.com>
2022-09-19 18:43:13 -0500

    updated version

--- rust/cosmic/cosmic-universe/src/lib.rs
+++ rust/cosmic/cosmic-universe/src/lib.rs
@@ -68,7 +68,7 @@ pub mod util;
 pub mod wave;
 
 lazy_static! {
-    pub static ref VERSION: semver::Version = semver::Version::from_str(include_str!("../../../../VERSION").trim()).unwrap();
+    pub static ref VERSION: semver::Version = semver::Version::from_str(include_str!("VERSION").trim()).unwrap();
     pub static ref HYPERUSER: Point = Point::from_str("hyperspace:users:hyperuser").expect("point");
     pub static ref ANONYMOUS: Point = Point::from_str("hyperspace:users:anonymous").expect("point");
 }
