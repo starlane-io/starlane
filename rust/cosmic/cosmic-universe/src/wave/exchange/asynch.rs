3e66e410440fb4bdd838327631c079ac5787fc55
Scott Williams <scott@mightydevco.com>
2022-09-25 02:55:38 -0500

    added AsyncRouter

--- rust/cosmic/cosmic-universe/src/wave/exchange/asynch.rs
+++ rust/cosmic/cosmic-universe/src/wave/exchange/asynch.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use crate::wave::exchange::{BroadTxRouter, TxRouter};
 use crate::wave::UltraWave;
 
@@ -18,4 +19,23 @@ impl Router for BroadTxRouter {
 #[async_trait]
 pub trait Router: Send + Sync {
     async fn route(&self, wave: UltraWave);
+}
+
+pub struct AsyncRouter {
+    pub router: Arc<dyn Router>
+}
+
+impl AsyncRouter {
+    pub fn new( router: Arc<dyn Router>) -> Self {
+        Self {
+            router
+        }
+    }
+}
+
+#[async_trait]
+impl Router for AsyncRouter {
+    async fn route(&self, wave: UltraWave) {
+        self.router.route(wave).await
+    }
 }
