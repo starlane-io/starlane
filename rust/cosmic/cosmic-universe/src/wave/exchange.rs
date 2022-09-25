81bd088e98fd268c81d6444d9e0e92ecbeb4836f
Scott Williams <scott@mightydevco.com>
2022-09-25 03:24:55 -0500

    prep_direct/prep_reflect

--- rust/cosmic/cosmic-universe/src/wave/exchange.rs
+++ rust/cosmic/cosmic-universe/src/wave/exchange.rs
@@ -6,25 +6,24 @@ use std::ops::Deref;
 use std::sync::Arc;
 use std::time::Duration;
 
+use asynch::Router;
 use dashmap::DashMap;
 use tokio::sync::{broadcast, mpsc, oneshot};
-use asynch::Router;
 
 use crate::config::bind::RouteSelector;
-use crate::loc::{Topic, ToPoint, ToSurface};
+use crate::loc::{ToPoint, ToSurface, Topic};
 use crate::log::{PointLogger, RootLogger, SpanLogger};
 use crate::settings::Timeouts;
-use crate::wave::core::CoreBounce;
-use crate::wave::{
-    Bounce, BounceBacks, BounceProto, DirectedProto, DirectedWave, Echo, FromReflectedAggregate,
-    Handling, Pong, Recipients, RecipientSelector, ReflectedAggregate, ReflectedProto,
-    ReflectedWave, Scope, Session, ToRecipients, UltraWave, Wave, WaveId,
-};
-use crate::{Agent, Point, ReflectedCore, Substance, Surface, ToSubstance, UniErr, wave};
 use crate::wave::core::cmd::CmdMethod;
 use crate::wave::core::http2::StatusCode;
+use crate::wave::core::CoreBounce;
 use crate::wave::exchange::asynch::AsyncRouter;
-
+use crate::wave::{
+    Bounce, BounceBacks, BounceProto, DirectedProto, DirectedWave, Echo, FromReflectedAggregate,
+    Handling, Pong, RecipientSelector, Recipients, ReflectedAggregate, ReflectedProto,
+    ReflectedWave, Scope, Session, ToRecipients, UltraWave, Wave, WaveId,
+};
+use crate::{wave, Agent, Point, ReflectedCore, Substance, Surface, ToSubstance, UniErr};
 
 #[derive(Clone)]
 pub struct Exchanger {
@@ -585,13 +584,104 @@ impl ProtoTransmitter {
         }
     }
 
-        pub async fn direct<D, W>(&self, wave: D) -> Result<W, UniErr>
+    pub async fn direct<D, W>(&self, wave: D) -> Result<W, UniErr>
     where
         W: FromReflectedAggregate,
         D: Into<DirectedProto>,
     {
         let mut wave: DirectedProto = wave.into();
 
+        self.prep_direct(&mut wave);
+
+        let directed = wave.build()?;
+
+        match directed.bounce_backs() {
+            BounceBacks::None => {
+                self.router.route(directed.to_ultra()).await;
+                FromReflectedAggregate::from_reflected_aggregate(ReflectedAggregate::None)
+            }
+            _ => {
+                let reflected_rx = self.exchanger.exchange(&directed).await;
+                self.router.route(directed.to_ultra()).await;
+                let reflected_agg = reflected_rx.await?;
+                FromReflectedAggregate::from_reflected_aggregate(reflected_agg)
+            }
+        }
+    }
+
+    pub async fn bounce_from(&self, to: &Surface, from: &Surface) -> bool {
+        let mut directed = DirectedProto::ping();
+        directed.from(from.clone());
+        directed.to(to.clone());
+        directed.method(CmdMethod::Bounce);
+        match self.direct(directed).await {
+            Ok(pong) => {
+                let pong: Wave<Pong> = pong;
+                pong.is_ok()
+            }
+            Err(_) => false,
+        }
+    }
+
+    pub async fn bounce(&self, to: &Surface) -> bool {
+        let mut directed = DirectedProto::ping();
+        directed.to(to.clone());
+        directed.method(CmdMethod::Bounce);
+        match self.direct(directed).await {
+            Ok(pong) => {
+                let pong: Wave<Pong> = pong;
+                pong.is_ok()
+            }
+            Err(_) => false,
+        }
+    }
+
+    pub async fn route(&self, wave: UltraWave) {
+        self.router.route(wave).await
+    }
+
+    pub async fn reflect<W>(&self, wave: W) -> Result<(), UniErr>
+    where
+        W: Into<ReflectedProto>,
+    {
+        let mut wave: ReflectedProto = wave.into();
+
+        self.prep_reflect(&mut wave);
+
+        let wave = wave.build()?;
+        let wave = wave.to_ultra();
+        self.router.route(wave).await;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+pub struct ProtoTransmitterDef<R> {
+    agent: SetStrategy<Agent>,
+    scope: SetStrategy<Scope>,
+    handling: SetStrategy<Handling>,
+    from: SetStrategy<Surface>,
+    to: SetStrategy<Recipients>,
+    router: R,
+    exchanger: Exchanger,
+}
+
+impl<R> ProtoTransmitterDef<R> {
+    pub fn from_topic(&mut self, topic: Topic) -> Result<(), UniErr> {
+        self.from = match self.from.clone() {
+            SetStrategy::None => {
+                return Err(UniErr::from_500(
+                    "cannot set Topic without first setting Surface",
+                ));
+            }
+            SetStrategy::Fill(from) => SetStrategy::Fill(from.with_topic(topic)),
+            SetStrategy::Override(from) => SetStrategy::Override(from.with_topic(topic)),
+        };
+        Ok(())
+    }
+
+    fn prep_direct(&self, wave: &mut DirectedProto) {
         match &self.from {
             SetStrategy::None => {}
             SetStrategy::Fill(from) => wave.fill_from(from.clone()),
@@ -621,61 +711,9 @@ impl ProtoTransmitter {
             SetStrategy::Fill(handling) => wave.fill_handling(handling),
             SetStrategy::Override(handling) => wave.handling(handling.clone()),
         }
-
-        let directed = wave.build()?;
-
-        match directed.bounce_backs() {
-            BounceBacks::None => {
-                self.router.route(directed.to_ultra()).await;
-                FromReflectedAggregate::from_reflected_aggregate(ReflectedAggregate::None)
-            }
-            _ => {
-                let reflected_rx = self.exchanger.exchange(&directed).await;
-                self.router.route(directed.to_ultra()).await;
-                let reflected_agg = reflected_rx.await?;
-                FromReflectedAggregate::from_reflected_aggregate(reflected_agg)
-            }
-        }
     }
 
-    pub async fn bounce_from(&self, to: &Surface, from: &Surface ) -> bool {
-        let mut directed = DirectedProto::ping();
-        directed.from(from.clone());
-        directed.to(to.clone());
-        directed.method(CmdMethod::Bounce);
-        match self.direct(directed).await {
-            Ok(pong) => {
-                let pong : Wave<Pong> = pong;
-                pong.is_ok()
-            },
-            Err(_) => false
-        }
-    }
-
-
-    pub async fn bounce(&self, to: &Surface ) -> bool {
-        let mut directed = DirectedProto::ping();
-        directed.to(to.clone());
-        directed.method(CmdMethod::Bounce);
-        match self.direct(directed).await {
-            Ok(pong) => {
-                let pong : Wave<Pong> = pong;
-                pong.is_ok()
-            },
-            Err(_) => false
-        }
-    }
-
-    pub async fn route(&self, wave: UltraWave) {
-        self.router.route(wave).await
-    }
-
-    pub async fn reflect<W>(&self, wave: W) -> Result<(), UniErr>
-    where
-        W: Into<ReflectedProto>,
-    {
-        let mut wave: ReflectedProto = wave.into();
-
+    fn prep_reflect(&self, wave: &mut ReflectedProto) {
         match &self.from {
             SetStrategy::None => {}
             SetStrategy::Fill(from) => wave.fill_from(from),
@@ -699,56 +737,7 @@ impl ProtoTransmitter {
             SetStrategy::Fill(handling) => wave.fill_handling(handling),
             SetStrategy::Override(handling) => wave.handling(handling.clone()),
         }
-
-        let wave = wave.build()?;
-        let wave = wave.to_ultra();
-        self.router.route(wave).await;
-
-        Ok(())
     }
-}
-
-#[derive(Clone)]
-pub struct ProtoTransmitterDef<R> {
-    agent: SetStrategy<Agent>,
-    scope: SetStrategy<Scope>,
-    handling: SetStrategy<Handling>,
-    from: SetStrategy<Surface>,
-    to: SetStrategy<Recipients>,
-    router: R,
-    exchanger: Exchanger,
-}
-
-impl <R> ProtoTransmitterDef<R> {
-    /*
-    pub fn new(router: R, exchanger: Exchanger) -> ProtoTransmitter {
-        let router = AsyncRouter::new(Arc::new(router));
-        Self {
-            from: SetStrategy::None,
-            to: SetStrategy::None,
-            agent: SetStrategy::Fill(Agent::Anonymous),
-            scope: SetStrategy::Fill(Scope::None),
-            handling: SetStrategy::Fill(Handling::default()),
-            router,
-            exchanger,
-        }
-    }
-     */
-
-    pub fn from_topic(&mut self, topic: Topic) -> Result<(), UniErr> {
-        self.from = match self.from.clone() {
-            SetStrategy::None => {
-                return Err(UniErr::from_500(
-                    "cannot set Topic without first setting Surface",
-                ));
-            }
-            SetStrategy::Fill(from) => SetStrategy::Fill(from.with_topic(topic)),
-            SetStrategy::Override(from) => SetStrategy::Override(from.with_topic(topic)),
-        };
-        Ok(())
-    }
-
-
 }
 
 #[derive(Clone)]
