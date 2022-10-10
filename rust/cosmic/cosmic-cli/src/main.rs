c4a44e31412cf628653ad66701fe187fba81e0c4
Scott Williams <scott@mightydevco.com>
2022-10-09 22:24:09 -0500

    added repl to cosmic-cli to import scripts

--- rust/cosmic/cosmic-cli/src/main.rs
+++ rust/cosmic/cosmic-cli/src/main.rs
@@ -3,13 +3,16 @@
 #[macro_use]
 extern crate clap;
 
+#[macro_use]
+extern crate text_io;
+
 use clap::arg;
 use clap::command;
 use clap::{App, Arg, Args, Command as ClapCommand, Parser, Subcommand};
 use cosmic_hyperlane::test_util::SingleInterchangePlatform;
 use cosmic_hyperlane::HyperwayEndpointFactory;
 use cosmic_hyperlane_tcp::HyperlaneTcpClient;
-use cosmic_hyperspace::driver::control::ControlClient;
+use cosmic_hyperspace::driver::control::{ControlClient, ControlCliSession};
 use cosmic_space::err::UniErr;
 use cosmic_space::loc::{Point, ToSurface};
 use cosmic_space::log::RootLogger;
@@ -28,7 +31,7 @@ use cosmic_nom::new_span;
 
 #[tokio::main]
 async fn main() -> Result<(), UniErr> {
-    let matches = ClapCommand::new("comsic-cli")
+    let matches = ClapCommand::new("cosmic-cli")
         .arg(
             Arg::new("host")
                 .short('h')
@@ -46,85 +49,112 @@ async fn main() -> Result<(), UniErr> {
                 .value_name("certs")
                 .required(false)
                 .default_value("./certs"),
-        )
+        ).subcommand(ClapCommand::new("script"))
         .allow_external_subcommands(true)
         .get_matches();
 
+    let host = matches.get_one::<String>("host").unwrap().clone();
+    let certs = matches.get_one::<String>("certs").unwrap().clone();
+    let session = Session::new(host,certs).await?;
+
     if matches.subcommand_name().is_some() {
-        let host = matches.get_one::<String>("host").unwrap().clone();
-        let certs = matches.get_one::<String>("certs").unwrap().clone();
-        command(host, certs, matches.subcommand_name().unwrap()).await
+        session.command(matches.subcommand_name().unwrap()).await
     } else {
+        loop {
+            let line: String = text_io::try_read!("{};").map_err(|e|UniErr::new(500,"err"))?;
+
+            let line_str= line.trim();
+
+            if "exit" == line_str {
+                return Ok(());
+            }
+            println!("> {}",line_str);
+            session.command(line.as_str()).await?;
+        }
         Ok(())
     }
 }
 
-async fn command(host: String, certs: String, command: &str) -> Result<(), UniErr> {
-    let logger = RootLogger::default();
-    let logger = logger.point(Point::from_str("cosmic-cli")?);
-    let tcp_client: Box<dyn HyperwayEndpointFactory> = Box::new(HyperlaneTcpClient::new(
-        format!("{}:{}", host, 4343),
-        certs,
-        Knock::default(),
-        false,
-        logger,
-    ));
-
-    let client = ControlClient::new(tcp_client)?;
-    client.wait_for_ready(Duration::from_secs(5)).await?;
-
-    tokio::time::sleep(Duration::from_secs(1)).await;
-    let cli = client.new_cli_session().await?;
-
-
-    let blocks = result(upload_blocks(new_span(command)))?;
-    let mut command = RawCommand::new(command.to_string());
-    for block in blocks {
-        let content = Arc::new(fs::read(block.name.as_str()).await?);
-        command.transfers.push( CmdTransfer::new(block.name, content ));
-    }
-
-    let core = cli.raw(command).await?;
-    core_out(core);
-
-    Ok(())
+pub struct Session{
+    pub client: ControlClient,
+    pub cli: ControlCliSession
 }
 
-pub fn core_out( core: ReflectedCore ) {
-    match core.is_ok() {
-        true => out(core.body),
-        false => {out_err(core.ok_or().unwrap_err() )}
-    }
-}
+impl Session {
+    pub async fn new(host: String, certs: String) -> Result<Self,UniErr> {
+        let logger = RootLogger::default();
+        let logger = logger.point(Point::from_str("cosmic-cli")?);
+        let tcp_client: Box<dyn HyperwayEndpointFactory> = Box::new(HyperlaneTcpClient::new(
+            format!("{}:{}", host, 4343),
+            certs,
+            Knock::default(),
+            false,
+            logger,
+        ));
 
-pub fn out(  substance: Substance ) {
-    match substance {
-        Substance::Empty => {
-            println!("Ok");
+        let client = ControlClient::new(tcp_client)?;
+        client.wait_for_ready(Duration::from_secs(5)).await?;
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        let cli = client.new_cli_session().await?;
+
+        Ok(Self {
+            client,
+            cli
+        })
+    }
+
+    async fn command(&self, command: &str) -> Result<(), UniErr> {
+
+        let blocks = result(upload_blocks(new_span(command)))?;
+        let mut command = RawCommand::new(command.to_string());
+        for block in blocks {
+            let content = Arc::new(fs::read(block.name.as_str()).await?);
+            command.transfers.push(CmdTransfer::new(block.name, content));
         }
-        Substance::List(list) => {
-            for i in list.list {
-                out(*i);
+
+        let core = self.cli.raw(command).await?;
+        self.core_out(core);
+
+        Ok(())
+    }
+
+    pub fn core_out(&self, core: ReflectedCore) {
+        match core.is_ok() {
+            true => self.out(core.body),
+            false => { self.out_err(core.ok_or().unwrap_err()) }
+        }
+    }
+
+    pub fn out(&self, substance: Substance) {
+        match substance {
+            Substance::Empty => {
+                println!("Ok");
+            }
+            Substance::List(list) => {
+                for i in list.list {
+                    self.out(*i);
+                }
+            }
+            Substance::Point(point) => {
+                println!("{}", point.to_string());
+            }
+            Substance::Surface(surface) => {
+                println!("{}", surface.to_string());
+            }
+            Substance::Text(text) => {
+                println!("{}", text);
+            }
+            Substance::Stub(stub) => {
+                println!("{}<{}>", stub.point.to_string(), stub.kind.to_string())
+            }
+            what => {
+                eprintln!("cosmic-cli not sure how to output {}", what.kind().to_string())
             }
         }
-        Substance::Point(point) => {
-            println!("{}",point.to_string());
-        }
-        Substance::Surface(surface) => {
-            println!("{}",surface.to_string());
-        }
-        Substance::Text(text) => {
-            println!("{}",text);
-        }
-        Substance::Stub(stub) => {
-            println!("{}<{}>", stub.point.to_string(), stub.kind.to_string())
-        }
-        what => {
-            eprintln!("cosmic-cli not sure how to output {}",what.kind().to_string())
-        }
     }
-}
 
-pub fn out_err( err: UniErr ) {
-    eprintln!("{}",err.to_string())
+    pub fn out_err(&self, err: UniErr) {
+        eprintln!("{}", err.to_string())
+    }
 }
