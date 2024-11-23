06cd3fd330ac12ec2718ebdeb145fcb6817ee701
Scott Williams <scott@uberscott.com>
2024-11-23 15:07:06 -0600

    lots of serde and yaml learnings

--- starlane/src/hyperspace/foundation/util.rs
+++ starlane/src/hyperspace/foundation/util.rs
@@ -1,11 +1,13 @@
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::collections::HashMap;
 use std::fmt;
-use std::fmt::Write;
+use std::fmt::{Formatter, Write};
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
 use rustls::pki_types::Der;
+use serde::__private::de::missing_field;
 use serde::de::{DeserializeOwned, MapAccess, Visitor};
+use serde::ser::SerializeMap;
 use serde_yaml::Value::Tagged;
 /*
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
@@ -278,6 +280,207 @@ pub mod test {
 }
 
 
+
+/*
+impl Serialize for Thingy {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        let mut map = serializer.serialize_map(Some(1))?;
+        map.serialize_key("Postgres")?;
+    }
+}
+
+ */
+
+
+
+
+
+fn blah() -> &'static str {
+    "beware"
+}
+#[derive(Debug,Eq,PartialEq,Clone)]
+struct Thingy {
+    thingy: HashMap<String, String>,
+}
+impl Thingy {
+    fn new(map: HashMap<String, String>) -> Thingy {
+        Self {
+            thingy: map
+        }
+    }
+}
+
+
+    impl serde::Serialize for Thingy {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            let mut _serde_state = serde::Serializer::serialize_struct(serializer, "Thingy", false as usize + 1)?;
+            serde::ser::SerializeStruct::serialize_field(&mut _serde_state, "Registry", &self.thingy)?;
+            serde::ser::SerializeStruct::end(_serde_state)
+        }
+    }
+
+impl<'de> serde::Deserialize<'de> for Thingy {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[allow(non_camel_case_types)]
+        #[doc(hidden)]
+        enum Field { field0, ignore }
+        #[doc(hidden)]
+        struct FieldVisitor;
+        impl<'de> serde::de::Visitor<'de> for FieldVisitor {
+            type Value = Field;
+            fn expecting(&self, formatter: &mut Formatter) -> fmt::Result { Formatter::write_str(formatter, "field identifier") }
+            fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                match value {
+                    0u64 => Ok(Field::field0),
+                    _ => Ok(Field::ignore),
+                }
+            }
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                match value {
+                    "Registry" => Ok(Field::field0),
+                    _ => { Ok(Field::ignore) }
+                }
+            }
+            fn visit_bytes<E>(self, value: &[u8]) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                match value {
+                    b"Registry" => Ok(Field::field0),
+                    _ => { Ok(Field::ignore) }
+                }
+            }
+        }
+        impl<'de> serde::Deserialize<'de> for Field {
+            #[inline]
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            { serde::Deserializer::deserialize_identifier(deserializer, FieldVisitor) }
+        }
+        #[doc(hidden)]
+        struct Visitor<'de> {
+            marker: PhantomData<Thingy>,
+            lifetime: PhantomData<&'de ()>,
+        }
+        impl<'de> serde::de::Visitor<'de> for Visitor<'de> {
+            type Value = Thingy;
+            fn expecting(&self, formatter: &mut Formatter) -> fmt::Result { Formatter::write_str(formatter, "struct Thingy") }
+            #[inline]
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let field0 = match serde::de::SeqAccess::next_element::<HashMap<String, String>>(&mut seq)? {
+                    Some(value) => value,
+                    None => return Err(serde::de::Error::invalid_length(0usize, &"struct Thingy with 1 element")),
+                };
+                Ok(Thingy {
+                    thingy: field0
+                })
+            }
+            #[inline]
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::MapAccess<'de>,
+            {
+                let mut field0: Option<HashMap<String, String>> = None;
+                while let Some(key) = serde::de::MapAccess::next_key::<Field>(&mut map)? {
+                    match key {
+                        Field::field0 => {
+                            if Option::is_some(&field0) { return Err(<A::Error as serde::de::Error>::duplicate_field("Registry")); }
+                            field0 = Some(serde::de::MapAccess::next_value::<HashMap<String, String>>(&mut map)?);
+                        }
+                        _ => { let _ = serde::de::MapAccess::next_value::<serde::de::IgnoredAny>(&mut map)?; }
+                    }
+                }
+                let field0 = match field0 {
+                    Some(field0) => field0,
+                    None => missing_field("Registry")?,
+                };
+                Ok(Thingy {
+                    thingy: field0
+                })
+            }
+        }
+        #[doc(hidden)]
+        const FIELDS: &'static [&'static str] = &["Registry"];
+        serde::Deserializer::deserialize_struct(deserializer, "Thingy", FIELDS, Visitor { marker: PhantomData::<Thingy>, lifetime: PhantomData })
+    }
+}
+
+
+#[test]
+fn thingy() {
+    let mut map = HashMap::new();
+    map.insert("hello".to_string(), "doctor".to_string());
+    map.insert("yesterday".to_string(), "tomorrow".to_string());
+   let thingy = Thingy::new(map);
+    let list = vec![thingy.clone(),thingy.clone(),thingy.clone()];
+    println!("{}", serde_yaml::to_string(&list).unwrap());
+}
+
+
+
+#[test]
+fn des_thingy() {
+    let mut map = HashMap::new();
+    map.insert("hello".to_string(), "doctor".to_string());
+    map.insert("yesterday".to_string(), "tomorrow".to_string());
+    let thingy = Thingy::new(map);
+    let list = vec![thingy.clone(),thingy.clone(),thingy.clone()];
+
+    let yaml =
+r#"
+- Registry:
+    yesterday: tomorrow
+    hello: doctor
+- Registry:
+    yesterday: tomorrow
+    hello: doctor
+- Registry:
+    yesterday: tomorrow
+    hello: doctor
+"#;
+
+   let parsed  =serde_yaml::from_str::<Vec<Thingy>>(yaml).unwrap();
+
+    assert_eq!(list,parsed);
+
+
+let yaml =
+        r#"
+- Registry:
+  yesterday: tomorrow
+  hello: doctor
+- Registry:
+  yesterday: tomorrow
+  hello: doctor
+- Registry:
+  yesterday: tomorrow
+  hello: doctor
+"#;
+
+
+    let parsed2  =serde_yaml::from_str::<Vec<Thingy>>(yaml).unwrap();
+    println!("{}", serde_yaml::to_string(&parsed2).unwrap());
+
+}
 
 
 
